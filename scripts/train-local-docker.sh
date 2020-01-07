8635f45b4df0eb877151791230df7dd056109db9
Aleksey Vlasenko <alekseyv@google.com>
2020-01-07 14:02:42 -0800

    added --gpus all and comment to run install NVIDIA driver and container toolkit

--- scripts/train-local-docker.sh
+++ scripts/train-local-docker.sh
@@ -5,5 +5,7 @@ export IMAGE_TAG=v1
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 docker build -f Dockerfile -t $IMAGE_URI ./
 docker push $IMAGE_URI
-docker run $IMAGE_URI python trainer/trainer.py --job-dir=/root/model $@
+
+# Install NVIDIA driver and https://github.com/NVIDIA/nvidia-docker in order to be able to train models on GPU.
+docker run --gpus all $IMAGE_URI python trainer/trainer.py --job-dir=/root/model $@
 #docker run -it -v ${PWD}:/host $IMAGE_URI python trainer/trainer.py --job-dir=/root/model $@
